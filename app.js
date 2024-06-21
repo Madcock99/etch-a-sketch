dffa9b4a614154b803bc197b41812d464fb462bd
Madcock99 <mathew.adcock@outlook.com>
2024-06-20 22:47:49 -0400

    more updates

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+(function(document, window, undefined){
+
 function createGrid(number) {
     const gridContainer = document.querySelector(".grid-container");
     gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
-    for (let i = 1; 1<= number*number; i++) {
+    for (let i = 1; i <= number*number; i++) {
         let div = document.createElement("div");
         div.classList.add("grid-box")
         gridContainer.appendChild(div);
     }
-    document.querySelector("grid-dimensions").textContent = `${number}x${number}`;
+    document.querySelector(".grid-dimensions").textContent = `${number}x${number}`;
 }
+
+
+let gridNumber = 16;
+createGrid(gridNumber);
+
+
+})(document, window)
