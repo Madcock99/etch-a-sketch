898561d59e051c0987437c98e86081dcf153c2ed
Madcock99 <mathew.adcock@outlook.com>
2024-06-25 22:01:44 -0400

    validated changecolor function; ;

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ function createGrid(number) {
 }
 
 function randomBgColor() {
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
     return "rgb(" + x + "," + y + "," + z + ")";
 
 }
